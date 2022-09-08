b6726373978e52a957952429bd661a656ea67789
Benedek Bálint Tóth <benedek.toth@bookrkids.hu>
2022-09-08 14:28:57 +0200

    upload 2022-09-08 14-28

--- index.js
+++ index.js
@@ -220,6 +220,7 @@ function BookDataRecived(jsonData, isLoggedIn)
     
     if (bookId){
         videoPlayerBoyHtml.hidden = false;
+		videoPlayerBoyHtml.style.display = "block";
         bookListHtmlItem.remove();
     } else {
         bookListHtmlItem.hidden = false;
@@ -261,6 +262,10 @@ function BookDataRecived(jsonData, isLoggedIn)
 
 function LoadMobile()
 {
+	bookListHtmlItem.hidden = true;
+	videoPlayerBoyHtml.hidden = true;
+	myVideoHtml.pause();
+	
 	const params = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
     });
@@ -335,9 +340,9 @@ function LoadMobile()
         BookDataRecived(jsonData, false);
     });
 
-    bookListHtmlItem.hidden = true;
-    videoPlayerBoyHtml.hidden = true;
-    myVideoHtml.pause();
+    //bookListHtmlItem.hidden = true;
+    //videoPlayerBoyHtml.hidden = true;
+    //myVideoHtml.pause();
     /* Custom Progressbar - Don't delete it, maybe it can be useful later again - Client asked for it, then changed his mind!
     document.getElementById("my-video").addEventListener("timeupdate", function() {
     // if the video is loaded and duration is known

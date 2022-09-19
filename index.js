fabdce05645a4ff275e95e8e5de650cbcee7d901
Benedek Bálint Tóth <benedek.toth@bookrkids.hu>
2022-09-19 11:32:58 +0200

    upload 2022-09-19 11-32

--- index.js
+++ index.js
@@ -25,12 +25,11 @@ function myStartHandler(e) {
     if (!isCurrentBookFree) {
         console.log("Book is not free!");
         myVideoHtml.pause();
-        //alert("This book is not free!");
-		$( document ).ready(function() {
-			console.log( "ready!" );
-			('#bookNotFreeModal').modal('show');
+        //alert("This book is not free!");		
+		window.onload = function() {
+            $('#bookNotFreeModal').modal('show');
 			$('.modal-body').html('Subscribe to see this book!');
-		});
+        }
         /*var href = window.location.href;
         window.location.href = href.split('?')[0];*/
     }
